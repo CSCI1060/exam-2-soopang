84d2be7b8dcdcabcbe9f602bc195e0a5bd4a2f18
soopang <shuang36@slu.edu>
2024-11-11 03:39:14 -0600

    Update fruit.m
    added code to fruit.m

--- fruit.m
+++ fruit.m
@@ -1,7 +1,35 @@
-function [] = fruit( N )
-% This function prints for values from 1 to N:
-% “apple” if the value is divisible by 2
-% “banana” if the value is divisible by 5
-% The number itself if neither is true
-% “applebanana” if both are true
+%This function prints for values 1 to N
+%"apple" if the value if divisible by 2
+%"banana if the value is divisible by 5
+%The number itself if neither are true
+%"applebanana" if both are true
 
+
+function fruit(N)
+
+for i = 1:N
+
+    remtwo = rem(i,2);
+    remfive = rem(i,5);
+
+    if remtwo ==0 && remfive~=0
+
+fprintf("apple\n")
+    elseif remfive==0 && remtwo~=0
+fprintf("banana\n")
+
+    elseif remfive==0 && remtwo==0
+
+        fprintf("applebanana\n")
+
+    elseif remfive~=0 &&remtwo~=0
+fprintf("%d\n",i)
+
+    end
+
+
+end
+
+
+
+end
